27185d1e7fcad513e44f9a3cf7fa052c3dc5ae81
Calvin Pang <cal.pang123@gmail.com>
2024-02-01 18:12:54 +1100

    first commit

---
+++ src/main/java/com/medium/mediumdemo/MediumDemoApplication.java
@@ -0,0 +1,13 @@
+package com.medium.mediumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medium/mediumdemo/MediumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.mediumdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
